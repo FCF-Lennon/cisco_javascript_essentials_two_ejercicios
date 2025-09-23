ad68e81cad468589c4260c4d6ec660cc81f5f0ba
pipedev <fcf.remote@gmail.com>
2025-09-22 21:30:07 -0300

    docs(js): examen modulo 1 - curso Python Essentials II (Cisco): en progreso (4/19)

---
+++ modulo-01-objetos-sin-clases/modulo-01-examen.js
@@ -0,0 +1,80 @@
+
+// Questionario:
+
+
+// Pregunta 01 - Has declarado la siguiente instrucción: 
+// console.log(user.name)
+
+// ¿Qué declaración debería preceder a la instrucción para que el texto 
+// Bob aparezca en la consola como resultado de la ejecución del código?
+
+/*
+    Opciones:
+
+    - let user = [name: 'Bob'];
+    - let user = [name: 'Bob', age: 30];
+    - let user = {name: 'Bob', age: 30};
+    - let user = {name: 'Bob'}; / Respuesta
+*/
+
+
+// Pregunta 02 - Un objeto que ya no será utilizado:
+
+/* 
+    Opciones: 
+
+    - Se eliminará automáticamente. / Respuesta. 
+    - Debe eliminarse usando el comando delete.  
+    - Debe eliminarse asignando undefined a la variable que lo contiene. 
+    - Debe eliminarse asignando null a la variable que lo contiene.  
+*/
+
+
+// Pregunta 03 - Falta una línea en el siguiente código:
+
+/*
+    let car = {
+        make: 'Dodge',
+        model: 'Dakota'
+    }
+
+    // Inserta aquí la línea de código.
+    console.log(`${car.make} ${car.model}, color: ${car.color}`);
+*/
+
+// Selecciona la línea faltante correcta para que la consola muestre lo siguiente al ejecutar todo el código: 
+// Salida: Dodge Viper, color: red
+
+/* 
+    Opciones: 
+
+    - car.model = 'Viper'; car.color = 'red'; / Respuesta
+    - car.set('model', 'Viper'; car.add('color', 'red'); 
+    - Una línea vacía - no es posible añadir una nueva propiedad color a un objeto previamente creado; 
+    - car = {model: 'Viper', color: 'red'; 
+*/
+
+
+// Pregunta 04 - Has declarado un objeto circle: 
+
+let circle = {
+    centre: {
+        x: 10,
+        y: 20
+    },
+    radius: 100
+}
+
+
+// ¿Cuál de las siguientes opciones mostrará en la consola el valor de 
+// la coordenada x del círculo (es decir, 10)?
+
+/* 
+    Opciones: 
+
+    - console.log(centre.x); 
+    - console.log(circle.centre); 
+    - console.log(circle.x); 
+    - console.log(circle.centre.x); / Respuesta
+*/
+
