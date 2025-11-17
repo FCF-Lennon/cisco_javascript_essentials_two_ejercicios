bb1c61e7f19b84908761d0b79c18ff5e2589687f
pipedev <fcf.remote@gmail.com>
2025-11-16 22:10:42 -0300

    docs(js): examen modulo 3 - curso Python Essentials II (Cisco): en progreso (5/24)

---
+++ modulo-03-objetos-integrados/modulo-03-examen.js
@@ -0,0 +1,76 @@
+// Pregunta 01 - Analiza el siguiente código:
+
+console.log(`${Boolean(-1)} ${Boolean(0)}  ${String(0)}  ${String(1)} `);
+
+// ¿Qué aparecerá en la consola como resultado de la ejecución del código?
+
+/*
+    Opciones:
+
+    - true false false true
+    - false false 0 1
+    - false false underfined underfined
+    - true false 0 1 / Respuesta
+*/
+
+
+// Pregunta 02 - Falta una línea en el siguiente código:
+
+let str = 'sample string';
+// ...
+console.log(result);
+
+// Selecciona la línea correcta que falta para que el código ejecutado
+// produzca la siguiente salida en consola: ['SAMPLE', 'STRING']
+
+/*
+    Opciones:
+
+    - let result = str.split(' ').toUpperCase();
+    - let result = str.toUpperCase(split(' '));
+    - let result = str.toUpperCase().split(' '); / Respuesta
+    - let result = str.toUpperCase(' ');
+*/
+
+
+// Pregunta 03 - Analiza el siguiente código:
+
+str = 'sample string';
+console.log(`${str.includes('s')} ${str.indexOf('s')} ${str.lastIndexOf('s')}`);
+
+// ¿Qué aparecerá en la consola como resultado de la ejecución?
+
+/*
+    Opciones:
+
+    - 0 0 7 
+    - true 1 8 
+    - true 0 7 / Respuesta
+    - 1 1 8
+*/
+
+
+// Pregunta 04 - Una llamada a Date.now() devolverá:
+
+/*
+    Opciones:
+
+    - Un objeto de la clase Date que contiene la fecha y hora actuales. 
+    - El número de milisegundos desde las 00:00:00 del 1 de enero de 2000.
+    - El número de segundos desde las 00:00:00 del 1 de enero de 1970.
+    - El número de milisegundos desde las 00:00:00 del 1 de enero de 1970. / Respuesta
+*/
+
+
+// Pregunta 05 - Obtener el año actual puede realizarse con el comando:
+
+/*
+    Opciones:
+
+    - let year = Date.now().getFullYear(); 
+    - let year = (new Date(Date.now())).getFullYear(); / Respuesta
+    - let year = Date.getFullYear();
+    - let year = (new.Date.now().getFullYear();
+*/
+
+
