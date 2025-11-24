884ff2685d17ebde33c7bc3a56200fb67b7a83dc
pipedev <fcf.remote@gmail.com>
2025-11-24 11:12:53 -0300

    docs(js): modulo 4/seccion 0 - curso JavaScript Essentials Two (Cisco/contenido/finalizado)

---
+++ modulo-04-uso-avanzado-de-funciones/seccion-0-un-poco-mas-sobre-funciones.js
@@ -0,0 +1,22 @@
+
+// Un poco más sobre funciones:
+
+/*
+    Durante la primera parte de este curso aprendiste sobre las funciones
+    en JavaScript, y ahora estarás trabajando con funciones en la mayoría
+    de los temas que siguen.
+
+    Después de completar la primera parte del curso, ahora deberías ser
+    capaz de declarar una función, usar una expresión de función o crear
+    una función flecha.
+
+    Además, no deberías tener ningún problema para llamar funciones,
+    pasarles argumentos o devolver sus resultados.
+
+    Sin embargo, las funciones en JavaScript dan a los programadores muchas
+    más posibilidades.
+
+    Para poder usarlas plenamente, intentemos ampliar nuestro conocimiento
+    del tema, comenzando con cosas simples como los valores predeterminados
+    de los parámetros y terminando con la ejecución asíncrona.
+*/
